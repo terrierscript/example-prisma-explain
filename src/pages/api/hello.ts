3467b799c489666176f79903305d871122b4b355
S. Suzuki <terrierscript@gmail.com>
2022-08-30 23:02:05 +0900

    add

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -27,8 +27,26 @@ const explainQuery = async (prisma: PrismaClient, event: Prisma.QueryEvent) => {
   const rawQuery = SqlString.format(event.query, params)
 
   const explain = `EXPLAIN ${rawQuery}`
-  const explainResult = await prisma.$queryRawUnsafe(explain)
-  return explainResult
+  const explainResults = await prisma.$queryRawUnsafe(explain)
+  if (!Array.isArray(explainResults)) {
+    return []
+  }
+  return explainResults.map(row => {
+    return {
+      "id": row["f0"],
+      "select_type": row["f1"],
+      "table": row["f2"],
+      "partitions": row["f3"],
+      "type": row["f4"],
+      "possible_keys": row["f5"],
+      "key": row["f6"],
+      "key_len": row["f7"],
+      "ref": row["f8"],
+      "rows": row["f9"],
+      "filtered": row["f10"],
+      "extra": row["f11"]
+    }
+  })
 }
 
 const handler: NextApiHandler = async (req, res) => {
@@ -40,10 +58,6 @@ const handler: NextApiHandler = async (req, res) => {
     const result = await explainQuery(prisma, event)
     console.log(result)
   })
-  // prisma.$use(async (params, next) => {
-  //   console.log("middleware", params)
-  //   return next(params)
-  // })
 
   const data = await prisma.post.findFirst({
     where: {
@@ -54,6 +68,7 @@ const handler: NextApiHandler = async (req, res) => {
       }
     }
   })
+
   res.json({ name: 'John Doe', data })
   await prisma.$disconnect()
 }
