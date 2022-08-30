e283b6eded16d15b8f12e27da9f0b0cbda67b665
S. Suzuki <terrierscript@gmail.com>
2022-08-30 22:49:42 +0900

    add

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,35 @@
 import { Prisma, PrismaClient } from "@prisma/client"
 import { NextApiHandler } from "next"
+import * as SqlString from "sqlstring"
 
+import * as csvParse from "csv-parse"
+import * as Papa from "papaparse"
 
+const parseParam = (param: string) => {
+  const removeBlacket = param
+    .replace(/^\[/, "")
+    .replace(/\]$/, "")
+  const parsed = Papa.parse(removeBlacket, {
+    dynamicTyping: true
+  })
+  return parsed.data[0]
+}
+
+const explainQuery = async (prisma: PrismaClient, event: Prisma.QueryEvent) => {
+  if (!event.query.startsWith("SELECT")) {
+    return
+  }
+  const params = parseParam(event.params)
+
+  if (!Array.isArray(params)) {
+    return
+  }
+  const rawQuery = SqlString.format(event.query, params)
+
+  const explain = `EXPLAIN ${rawQuery}`
+  const explainResult = await prisma.$queryRawUnsafe(explain)
+  return explainResult
+}
 
 const handler: NextApiHandler = async (req, res) => {
   res.statusCode = 200
@@ -9,27 +37,21 @@ const handler: NextApiHandler = async (req, res) => {
     log: [{ level: "query", emit: "event" }]
   })
   prisma.$on("query", async (event) => {
-    console.log(event)
-    if (!event.query.startsWith("SELECT")) {
-      return
-    }
-    const params = JSON.parse(event.params)
-    if (!Array.isArray(params)) {
-      return
-    }
-    const replaced = params.reduce((query, param, idx) => {
-      const replaced = typeof param === "number" ? param : `"${param}"`
-      return query.replace("?", replaced)
-    }, event.query)
-
-    const explain = `EXPLAIN ${replaced}`
-    const explainResult = await prisma.$queryRawUnsafe(explain)
-    console.log({ explain, explainResult })
+    const result = await explainQuery(prisma, event)
+    console.log(result)
   })
+  // prisma.$use(async (params, next) => {
+  //   console.log("middleware", params)
+  //   return next(params)
+  // })
 
   const data = await prisma.post.findFirst({
     where: {
-      title: "xxx"
+      title: "xx,x",
+      published: true,
+      createdAt: {
+        gt: new Date().toISOString()
+      }
     }
   })
   res.json({ name: 'John Doe', data })
